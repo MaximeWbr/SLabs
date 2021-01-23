d13944527407645452c7d187c808e6f20e85dbae
Maxime <webermax@free.fr>
2021-01-23 18:03:16 +0100

    Update main to create Toulouse DataBase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+from src.DataBase import *
+from src.config import *
 #List of URL for the city
 
 print("Welcome, Choose your city:\n(1) Toulouse\n")
@@ -6,7 +7,7 @@ choise = input('Enter your answer: ')
 if choise == "1" or choise == "Toulouse":
 	print("You have chosen Toulouse.\n")
 	# Get the corresponding data bases
-		#Down load the D.Bs
+	toulouseDB = DataBase("Toulouse", TOULOUSE_URL_LIST, TOULOUSE_NAME_LSIT)
 	# Read the data bases
 	# Generate the website
 		# Once created ask to visit it
