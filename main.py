79e89e3d63a86a0901b9bf0a67e81bee3c557080
Maxime <webermax@free.fr>
2021-01-23 14:56:11 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+
+#List of URL for the city
+
+print("Welcome, Choose your city:\n(1) Toulouse\n")
+choise = input('Enter your answer: ')
+if choise == "1" or choise == "Toulouse":
+	print("You have chosen Toulouse.\n")
+	# Get the corresponding data bases
+		#Down load the D.Bs
+	# Read the data bases
+	# Generate the website
+		# Once created ask to visit it
+	# Show on the website
+		# City's data
+		# Ask for action (compare with Air Quality, Update)
+else:
+	print("Error: Wrong answer, code exit.")
+
+	# For more Cities
+		# Create a SQL data set to save D.B (Key = city name)
