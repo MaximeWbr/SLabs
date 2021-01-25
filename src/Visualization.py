5c58c21ce667785c8105c3f64f6711112888f065
Maxime <webermax@free.fr>
2021-01-25 15:39:04 +0100

    Create visualization class

---
+++ src/Visualization.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+class Visualization():
+
+	def __init__(self):
+
+	def _plot(self, name, data):
+		plt.plot(data)
+		plt.ylabel(name)
+		plt.show()
+
+	def _plotCompare(self, name1, data1, name2, data2):
+		plt.plot(data1, 'b', data2, 'r')
+		label = name1 + " compare to " + name2
+		plt.suptitle(label)
+		plt.show()
