317f596549b4b12a2a3d5a75a8d0c43425cd1e1e
Maxime <webermax@free.fr>
2021-02-02 23:18:24 +0100

    Get Air Quality DB and integration into the project

--- src/config.py
+++ src/config.py
@@ -1,4 +1,7 @@
 TOULOUSE_URL_LIST = ['https://www.data.gouv.fr/fr/datasets/r/264fe552-4f8a-4253-8afb-f0df65025fbf']
+AIR_URL = ['https://www.arcgis.com/sharing/rest/content/items/59b95f2f49c948519ebf58bc058ec7e2/data']
 TOULOUSE_PATH_DB = './data/Toulouse/'
+AIR_PATH_DB = './data/'
 TOULOUSE_PATH_GRAPH = "static/images/"
 TOULOUSE_NAME_LSIT= ['./data/Toulouse/Teso.csv']
+AIR_NAME= ['./data/AirQuality.csv']
--- webSite.py
+++ webSite.py
@@ -6,6 +6,8 @@ from src.mkdirPath import *
 import matplotlib.pyplot as plt
 import os
 
+#Global Variable to allow the DB update
+UPDATE_DB = 0
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -24,6 +26,10 @@ def single():
 def Compare():
 	return render_template('paramCompare.html')
 
+@app.route("/paramCompareAirQuality", methods=['POST', 'GET'])
+def AirCompare():
+	return render_template('paramCompareAirQuality.html')
+
 #Single Plot page
 @app.route('/plot', methods=['POST', 'GET'])
 def image():
@@ -33,13 +39,20 @@ def image():
 		label = request.form["data"]
 		s_number = int(request.form["sample_number"])
 
+	if city == "Toulouse":
+		output_dir = TOULOUSE_PATH_DB
+		urlList = TOULOUSE_URL_LIST
+		nameList = TOULOUSE_NAME_LSIT
+	else:
+		output_dir = AIR_PATH_DB
+		urlList = AIR_URL
+		nameList = AIR_NAME
 	# Create folder for data base
-	output_dir = TOULOUSE_PATH_DB
 	mkdir_p(output_dir)
 	# Get the corresponding data bases
-	toulouseDB = DataBase(city, TOULOUSE_URL_LIST, TOULOUSE_NAME_LSIT,0)
+	toulouseDB = DataBase(city, urlList, nameList,UPDATE_DB)
 	# Read the data bases
-	toulouseDB._getData(output_dir+"Teso.csv")
+	toulouseDB._getData(nameList[0])
 	data = toulouseDB._getSpecificData(label) #temperature_partie_decimale
 	# Plot data
 	visu = Visualization()
@@ -57,17 +70,73 @@ def imageCompare():
 		city2 = request.form["city2"]
 		label2 = request.form["data2"]
 		
+	if city1 == "Toulouse":
+		output_dir1 = TOULOUSE_PATH_DB
+		urlList1 = TOULOUSE_URL_LIST
+		nameList1 = TOULOUSE_NAME_LSIT
+	else:
+		output_dir1 = AIR_PATH_DB
+		urlList1 = AIR_URL
+		nameList1 = AIR_NAME
 
+	if city2 == "Toulouse":
+		output_dir2 = TOULOUSE_PATH_DB
+		urlList2 = TOULOUSE_URL_LIST
+		nameList2 = TOULOUSE_NAME_LSIT
+	else:
+		output_dir2 = AIR_PATH_DB
+		urlList2 = AIR_URL
+		nameList2 = AIR_NAME
 	# Create folder for data base
-	output_dir = TOULOUSE_PATH_DB
-	mkdir_p(output_dir)
+	mkdir_p(output_dir1)
+	mkdir_p(output_dir2)
 	# Get the corresponding data bases
-	toulouseDB = DataBase(city1, TOULOUSE_URL_LIST, TOULOUSE_NAME_LSIT,0)
+	dataBase1 = DataBase(city1, urlList1, nameList1, UPDATE_DB)
+	dataBase2 = DataBase(city2, urlList2, nameList2, UPDATE_DB)
 	# Read the data bases
-	toulouseDB._getData(output_dir+"Teso.csv")
-	print(toulouseDB.listeLabel)
-	data = toulouseDB._getSpecificData(label1)
-	data2 = toulouseDB._getSpecificData(label2)
+	dataBase1._getData(nameList1[0])
+	dataBase2._getData(nameList2[0])
+	data = dataBase1._getSpecificData(label1)
+	data2 = dataBase2._getSpecificData(label2)
+	# Plot data
+	visu = Visualization()
+	figName = visu._plotCompare(label1, data, label2, data2, s_number)
+	return render_template('plotCompare.html', image_path =figName)
+
+#The weather data and air quality compare page
+@app.route('/plotCompareAirQuality', methods=['POST', 'GET'])
+def imageAirCompare():
+	if request.method == 'POST':
+		result = request.form
+		city1 = request.form["city"]
+		label1 = request.form["data"]
+		s_number = int(request.form["sample_number"])
+		city2 = request.form["city2"]
+		label2 = "valeur"
+		
+	if city1 == "Toulouse":
+		output_dir1 = TOULOUSE_PATH_DB
+		urlList1 = TOULOUSE_URL_LIST
+		nameList1 = TOULOUSE_NAME_LSIT
+	else:
+		output_dir1 = AIR_PATH_DB
+		urlList1 = AIR_URL
+		nameList1 = AIR_NAME
+
+	output_dir2 = AIR_PATH_DB
+	urlList2 = AIR_URL
+	nameList2 = AIR_NAME
+	# Create folder for data base
+	mkdir_p(output_dir1)
+	mkdir_p(output_dir2)
+	# Get the corresponding data bases
+	dataBase1 = DataBase(city1, urlList1, nameList1, UPDATE_DB)
+	dataBase2 = DataBase(city2, urlList2, nameList2, UPDATE_DB)
+	# Read the data bases
+	dataBase1._getData(nameList1[0])
+	dataBase2._getDataAIR(nameList2[0])
+	data = dataBase1._getSpecificData(label1)
+	data2 = dataBase2._getSpecificData(label2)
 	# Plot data
 	visu = Visualization()
 	figName = visu._plotCompare(label1, data, label2, data2, s_number)
