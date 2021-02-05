44d840d91f852ca3b99367e6361d96e849789ce2
Maxime <webermax@free.fr>
2021-02-05 01:37:48 +0100

    Add Toulouse DB and Adapt

--- src/config.py
+++ src/config.py
@@ -1,12 +1,19 @@
-TOULOUSE_URL_LIST = ['https://www.data.gouv.fr/fr/datasets/r/264fe552-4f8a-4253-8afb-f0df65025fbf']
-AIR_URL = ['https://www.arcgis.com/sharing/rest/content/items/59b95f2f49c948519ebf58bc058ec7e2/data']
-TOULOUSE_PATH_DB = './data/Toulouse/'
-AIR_PATH_DB = './data/'
+TOULOUSE_URL_LIST = ["https://www.data.gouv.fr/fr/datasets/r/264fe552-4f8a-4253-8afb-f0df65025fbf",
+					"https://www.data.gouv.fr/fr/datasets/r/8b5d53af-011f-4518-b12c-54b3030db412",
+					"https://www.data.gouv.fr/fr/datasets/r/47f72f21-eba6-4620-810d-ebd3c228aaf0"]
+
+AIR_URL = "https://www.arcgis.com/sharing/rest/content/items/59b95f2f49c948519ebf58bc058ec7e2/data"
+
+TOULOUSE_PATH_DB = "./data/Toulouse/"
+AIR_PATH_DB = "./data/"
 TOULOUSE_PATH_GRAPH = "static/images/"
-TOULOUSE_NAME_LSIT= ['./data/Toulouse/Teso.csv']
-AIR_NAME= ['./data/AirQuality.csv']
+
+TOULOUSE_NAME_LSIT= ["./data/Toulouse/Teso.csv","./data/Toulouse/George_Sand.csv", "./data/Toulouse/La_Salade.csv"]
+AIR_NAME= "./data/AirQuality.csv"
+
+NAME_INDEX =["Toulouse_Tesco","Toulouse_George_Sand", "Toulouse_La_Salade"]
 
 #Global Variable to allow the DB update
 UPDATE_DB = 0
-TIME = 0
+TIME = [0,0,0]
 TIME_AIR = 0
